b33abaf887daeee4bfa98cdd897fd0b79b524fee
flohms18 <trouilletflorent@outlook.com>
2025-11-11 19:39:10 +0100

    Remove gutters

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -5,13 +5,23 @@ import Footer from './Footer.vue';
 </script>
 
 <template>
-  <Header></Header>
-  <h1 class="james">Dune</h1>
-  <Footer></Footer>
+  <div class="parallax">
+      <h1 class="james">DUNE</h1>
+  </div>
+  
 </template>
 
 <style scoped>
 .james {
   font-family: "custom_dune", sans-serif;
+}
+
+.parallax {
+  background-image: url("./src/assets/images/dune-wallpaper.jpeg");
+  min-height: 500px;
+  background-attachment: fixed;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
 }
 </style>
