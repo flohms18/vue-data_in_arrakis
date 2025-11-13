f363557168c525fe9e04d79ffc889c7e0788b1fb
flohms18 <trouilletflorent@outlook.com>
2025-11-13 14:14:13 +0100

    added Bootstrap

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,7 +2,7 @@
 </script>
 
 <template>
-    <h1 class="james">This is my header</h1>
+    <h1 class="james bg-danger">This is my header</h1>
 </template>
 
 <style scoped>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './components/App.vue'
 import './assets/styles/style.css';
 
+import 'bootstrap/dist/css/bootstrap.css'
 
 
 createApp(App).mount('#app')
