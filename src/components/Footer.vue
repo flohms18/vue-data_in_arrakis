7806675e1b3485cb5b1d29a24c51a61e49c4a71e
flohms18 <trouilletflorent@outlook.com>
2025-11-13 10:24:33 +0100

    Add New Background

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,7 +2,7 @@
 </script>
 
 <template>
-    <h1>This is my footer</h1>
+    <h1 class="james">This is my footer</h1>
 </template>
 
 <style scoped>
--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -5,23 +5,35 @@ import Footer from './Footer.vue';
 </script>
 
 <template>
-  <div class="parallax">
+  <Header></Header>
+  <div class="parallaxFirst">
       <h1 class="james">DUNE</h1>
+      <div class="toto">
+        James 
+      </div>
   </div>
-  
+  <Footer></Footer>
 </template>
 
 <style scoped>
-.james {
-  font-family: "custom_dune", sans-serif;
-}
 
-.parallax {
+
+.parallaxFirst {
   background-image: url("./src/assets/images/dune-wallpaper.jpeg");
   min-height: 500px;
   background-attachment: fixed;
   background-position: center;
   background-repeat: no-repeat;
   background-size: cover;
+}
+
+.toto {
+  height: 300px;
+  display: block;
+  text-align: center;
+  margin-left: auto;
+  margin-right: auto;
+  padding-top: 50px;
+  font-family: "custom_dune", sans-serif;
 }
 </style>
