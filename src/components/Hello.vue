fe94ff27a26000d0aae5b5663d5a9690d3956a00
flohms18 <trouilletflorent@outlook.com>
2025-11-14 10:38:50 +0100

    intro text completed

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -10,6 +10,8 @@ import Footer from './Footer.vue';
     <div class="Main_Title text-center">
       <h1 class="james">DATAKIS</h1>
       <p class="subtitle-dune">Those who understand data, shape the data-driven future</p>
+      <div class="intro-datakis w-50">
+ Spice is the most valuable resource in the universe. It enables interstellar travel, extends life, and grants the ability to foresee events through heightened prescience. Found only in significant quantities on the desert planet Arrakis, it is the foundation of power: whoever controls the Spice controls the galaxy.</div>
     </div>
   </div>
   <div class="parallaxSecond">
@@ -30,7 +32,7 @@ import Footer from './Footer.vue';
 
 .parallaxFirst {
   background-image: url("./src/assets/images/dune-wallpaper.jpeg");
-  min-height: 1000px;
+  min-height: 500px;
   background-attachment: fixed;
   background-position: center;
   background-repeat: no-repeat;
@@ -68,6 +70,23 @@ import Footer from './Footer.vue';
   padding-top: 100px;
 
 }
+
+.intro-datakis {
+  height: 100px;
+  display: block;
+  margin-left: auto;
+  font-size: 18px;
+  margin-right: auto;
+  font-family: "subtitle_dune_font", sans-serif
+
+}
+
+.subtitle-dune {
+    font-size: 24px;
+    font-family: "subtitle_dune_font", sans-serif
+  
+}
+
 
 
 
